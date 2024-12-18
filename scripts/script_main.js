0c7d8ce50c7ad49e88e6e88e6fc8346c50ac4732
LaLunaInSky <moonaflordapele@gmail.com>
2024-12-18 10:55:58 -0300

    18/12/2024 10:55

--- scripts/script_main.js
+++ scripts/script_main.js
@@ -1,1 +1,64 @@
 "use strict"
+
+let hora = 0
+let minuto = 0
+let segundo = 0 
+
+let mostradorContagem = document.querySelector('.mostradorContagem>h1')
+
+
+function formataçãoContagem() {
+    if (hora < 10) {
+        if (minuto < 10) {
+            if (segundo < 10) {
+                mostradorContagem.innerText = `0${hora} : 0${minuto} : 0${segundo}`
+            } else {
+                mostradorContagem.innerText = `0${hora} : 0${minuto} : ${segundo}`
+            }
+        } else {
+            if (segundo < 10) {
+                mostradorContagem.innerText = `0${hora} : ${minuto} : 0${segundo}`
+            } else {
+                mostradorContagem.innerText = `0${hora} : ${minuto} : ${segundo}`
+            }
+        }
+    } else {
+        if (minuto < 10) {
+            if (segundo < 10) {
+                mostradorContagem.innerText = `${hora} : 0${minuto} : 0${segundo}`
+            } else {
+                mostradorContagem.innerText = `${hora} : 0${minuto} : ${segundo}`
+            }
+        } else {
+            if (segundo < 10) {
+                mostradorContagem.innerText = `${hora} : ${minuto} : 0${segundo}`
+            } else {
+                mostradorContagem.innerText = `${hora} : ${minuto} : ${segundo}`
+            }
+        }
+    }
+}
+
+function contagem() {
+    segundo++
+
+    if (segundo > 60) {
+        minuto++
+        segundo = 0
+    }
+
+    if (minuto > 60) {
+        hora++
+        minuto = 0
+    }
+
+    if (hora > 99) {
+        hora = 0
+        minuto = 0
+        segundo = 0
+    }
+
+    formataçãoContagem()
+}
+
+setInterval(contagem, )

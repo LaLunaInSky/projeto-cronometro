ed95c9c32d541b9979a3ef9a308c541b9d20a7ce
LaLunaInSky <moonaflordapele@gmail.com>
2025-01-12 12:37:44 -0300

    12/01/2025 12:37

--- scripts/script_main.js
+++ scripts/script_main.js
@@ -6,39 +6,6 @@ let segundo = 0
 let mostradorContagem = document.querySelector('.mostradorContagem>h1')
 let  rodarContagem = false
 
-
-function formataçãoContagem() {
-    if (hora < 10) {
-        if (minuto < 10) {
-            if (segundo < 10) {
-                mostradorContagem.innerText = `0${hora} : 0${minuto} : 0${segundo}`
-            } else {
-                mostradorContagem.innerText = `0${hora} : 0${minuto} : ${segundo}`
-            }
-        } else {
-            if (segundo < 10) {
-                mostradorContagem.innerText = `0${hora} : ${minuto} : 0${segundo}`
-            } else {
-                mostradorContagem.innerText = `0${hora} : ${minuto} : ${segundo}`
-            }
-        }
-    } else {
-        if (minuto < 10) {
-            if (segundo < 10) {
-                mostradorContagem.innerText = `${hora} : 0${minuto} : 0${segundo}`
-            } else {
-                mostradorContagem.innerText = `${hora} : 0${minuto} : ${segundo}`
-            }
-        } else {
-            if (segundo < 10) {
-                mostradorContagem.innerText = `${hora} : ${minuto} : 0${segundo}`
-            } else {
-                mostradorContagem.innerText = `${hora} : ${minuto} : ${segundo}`
-            }
-        }
-    }
-}
-
 function contagem() {
     if (rodarContagem == true) {
         segundo++
@@ -60,7 +27,7 @@ function contagem() {
         segundo = 0
     }
 
-    formataçãoContagem()
+    mostradorContagem.innerText = `${hora < 10 ? `0${hora}` : hora} : ${minuto < 10 ? `0${minuto}` : minuto} : ${segundo < 10 ? `0${segundo}` : segundo}`
 }
 
 setInterval(contagem, 1000)
